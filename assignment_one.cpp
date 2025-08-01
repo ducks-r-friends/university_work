399837e548bcff4dd81dbe8ba4251992ba733618
Grace A <103120572+ducks-r-friends@users.noreply.github.com>
2025-08-01 17:14:56 +1200

    Update assignment_one.cpp

--- assignment_one.cpp
+++ assignment_one.cpp
@@ -1,28 +1,13 @@
 // Grace Appleton
-// 17094599
+//  17094599
 
 #include <iostream>
 using namespace std;
+    int difference, day, month, year, final_day, final_month, final_year, days_in_month, days_left_in_month; 
+    
+    int get_days_in_month(){
 
-int difference, day, month, year, final_day, final_month, final_year, days_in_month;
-
-int main()
-{
-    cout << "Enter difference in days: ";
-    cin >> difference;
-    cout << "Enter date in the format day month year:";
-    cin >> day >> month >> year;
-    if (difference < 0)
-    {
-        month = (month - 1);
-    }   
-
-    if (month == 1)
-    {
-        days_in_month = 31;
-    }
-    else if (month == 2)
-    {
+    if (month == 2){
         if ((year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0))
         {
             days_in_month = 29;
@@ -32,47 +17,28 @@ int main()
             days_in_month = 28;
         }
     }
-    else if (month == 3)
+    else if (month == 4 || month == 6 || month == 9 || month == 11)
     {
         days_in_month = 30;
     }
-    else if (month == 4)
-    {
-        days_in_month = 30;
-    }
-    else if (month == 5)
+    else 
     {
         days_in_month = 31;
     }
-    else if (month == 6)
-    {
-        days_in_month = 30;
     }
-    else if (month == 7)
-    {
-        days_in_month = 31;
-    }
-    else if (month == 8)
-    {
-        days_in_month = 31;
-    }
-    else if (month == 9)
-    {
-        days_in_month = 30;
-    }
-    else if (month == 10)
-    {
-        days_in_month = 31;
-    }
-    else if (month == 11)
-    {
-        days_in_month = 30;
-    }
-    else if (month == 12)
-    {
-        days_in_month = 31;
-    }
+
+    int main(){
+   
+    cout << "Enter difference in days: ";
+    cin >> difference;
+    cout << "Enter date in the format day month year:";
+    cin >> day >> month >> year;
     final_month = month; 
+    days_left_in_month = (days_in_month - day);
+
+    if (difference > 0) {
+        final_day = 
+    }
 
     if ((day + difference) > days_in_month)
     {
